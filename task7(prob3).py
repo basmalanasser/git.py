7f0656047037f713aa826583b65d1e14e17baeeb
Bassant <basantihab99@gmail.com>
2024-07-24 00:35:45 +0300

    Update task7(prob3).py

--- task7(prob3).py
+++ task7(prob3).py
@@ -1,76 +1,49 @@
-chats=[]
-users=[]
-
-def register_user(user):
-    users.append(user)
-
-
-def send_message(message):
-    chats.append(message)
-
-
-def view_messages():
+chats = {}
+username = input
+def user(username):
+    if username in chats :
+        return f"User '{username}' exists"
+    else:
+        user.append(username)
+        return f"User '{username}' registered successfully."
+def send_message(sender, receivers ,message):
+    return "Message sent successfully."
+    message = {
+        "sender": sender,
+        "receiver": receiver,
+        "message": message_text
+    }
+    chat_system[sender].append(message)
+    chat_system[receiver].append(message)
+    return f"Message sent from '{sender}' to '{receiver}'."
+def view_messages(username):
     for i in range(len(chats)):
         print(f"Chats:{chats[i]}")
-
-
-def view_users():
-    for i in range(len(users)):
-        print(f"Users: {users[i]}")
-
-
-def search_user(user):
-    search_user(user)
-    
-    
 def main():
     while True:
          print("1. Register user:")
          print("2. Send message:")
          print("3. View messages:")
-         print("4. Search for user:")
-         print("5. View users:")
-         print("6. Exit")
+         print("4. Exit")
          option=int(input("Enter an option:"))
-
          if option==1:
             user=input("Enter username:")
-            register_user(user)
+            user:{username}
             print(f"user: {user} registered successfully!")
-
-
          elif option==2:
             message=input("Enter a message:")
-            send_message(message)
+            send_message :{sender, receivers ,message}
             print(f"message: {message} sent successfully!")
-
-
          elif option==3:
-            view_messages()
-
-
+            view = input ("Enter your message :")
+            view_messages :{username}
+            print(f"view: {view} !")
 
          elif option==4:
-            user=input("Who are u searching for?")
-            if user in users:
-                print(f"user: {user} found successfully!")
-            else:
-                print(f"user: {user} not found!")
-
-
-         elif option==5:
-             view_users()
-
-
-         elif option==6:
             break
-
          else:
             print("invalid option!")
-
 main()
-
-    
 
 
 
